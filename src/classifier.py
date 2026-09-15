9ca49c1f3dcfa44b75eaf812344689f44ac32b40
AlexGames567 <beckal@katharineum.de>
2026-09-15 13:49:48 +0200

    Update classifier.py

--- src/classifier.py
+++ src/classifier.py
@@ -59,34 +59,30 @@ def classify_image(image_bytes: bytes) -> list[str]:
     Bei jedem Fehler (defektes Bild, Modellproblem, fehlende Abhaengigkeit)
     wird auf ['Sonstiges'] zurueckgefallen, damit die App nie abstuerzt.
     """
-    try:
-        model = load_model()
-        labels = load_labels()
+    model = load_model()
+    labels = load_labels()
 
         # Eingabegroesse wird dynamisch aus dem Modell gelesen, nicht hart codiert.
-        input_shape = model.input_shape
-        target_h = input_shape[1] or 224
-        target_w = input_shape[2] or 224
+    input_shape = model.input_shape
+    target_h = input_shape[1] or 224
+    target_w = input_shape[2] or 224
 
-        img = Image.open(io.BytesIO(image_bytes)).convert("RGB")
-        img = img.resize((target_w, target_h))
-        arr = np.asarray(img, dtype=np.float32)
+    img = Image.open(io.BytesIO(image_bytes)).convert("RGB")
+    img = img.resize((target_w, target_h))
+    arr = np.asarray(img, dtype=np.float32)
 
         # Normalisierung auf [-1, 1]: Standard bei Teachable-Machine-Exporten
         # (typisches Format fuer keras_model.h5 + labels.txt). Falls das
         # Modell anders trainiert wurde, muesste diese Zeile angepasst werden.
-        arr = (arr / 127.5) - 1.0
-        arr = np.expand_dims(arr, axis=0)
+    arr = (arr / 127.5) - 1.0
+    arr = np.expand_dims(arr, axis=0)
 
-        prediction = model.predict(arr, verbose=0)
-        print(prediction)
-        top_index = int(np.argmax(prediction[0]))
+    prediction = model.predict(arr, verbose=0)
+    print(prediction)
+    top_index = int(np.argmax(prediction[0]))
 
-        if 0 <= top_index < len(labels):
-            print("Funktioniert?")
-            return [labels[top_index]]
-        print("Fuck my Life")
-        return [FALLBACK_TAG]
-    except Exception:
-        print("Fehler")
-        return [FALLBACK_TAG]
+    if 0 <= top_index < len(labels):
+        print("Funktioniert?")
+        return [labels[top_index]]
+    print("Fuck my Life")
+    return [FALLBACK_TAG]
